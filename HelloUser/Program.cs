7056acb35122b313853a0240c676df9c253be408
Ilia «angu1ss» Antonov <t34.ddt@list.ru>
2021-01-15 23:25:29 +0300

    Initial commit

---
+++ HelloUser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace HelloUser
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Enter name");
+            var name  = Console.ReadLine();
+            Console.WriteLine($"Hey, {name}. Today is {DateTime.Now:dd.MM.yyyy}");
+            // User-friendly app finish
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to close application...");
+            Console.Read();
+        }
+    }
+}
